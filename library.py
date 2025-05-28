f333ac398764b2d6a407d40ca9856f2deba5e1ca
C0dave <cuchomaus@gmail.com>
2025-05-27 19:27:06 -0500

    updated

--- library.py
+++ library.py
@@ -3,9 +3,11 @@ import ttkbootstrap as ttk
 import tkinter.messagebox as messagebox
 import datetime
 import json
-from PIL import Image, ImageTk
 from colorama import Fore, init
+from PIL import Image, ImageTk
 import os
+import script as sc
+
 
 subject = ["Alejandro", "Santiago", "Mateo", "Mirian", "Jonathan"]
 
@@ -50,156 +52,43 @@ def json_data():
             print(Fore.GREEN + "✅ Archivo JSON creado con éxito")
     else:
         print(Fore.YELLOW + "\n⚠️ El archivo JSON ya existe")
-
-def save_json():    
-    global subject
-    with open("person.json", "r", encoding="utf-8") as file:
-        data = json.load(file)
-    if subject[num] in data and data != {}:
-        data[subject[num]]["faltas"].append(fault_details())
-        data[subject[num]]["dia"].append(date())
-        data[subject[num]]["num_de_faltas"] = len(data[subject[num]]["faltas"])
-
-        with open("person.json", "w", encoding="utf-8") as file:
-            json.dump(data, file, indent=4, ensure_ascii=False)     
-        print(Fore.GREEN + "\n✅ Datos guardados con éxito", Fore.CYAN + "INFO: ", Fore.LIGHTBLUE_EX + "\nRedaccion de falta: ",  fault_details(), Fore.LIGHTBLUE_EX + "\nNumero de falta: ", str(increment_number()), Fore.LIGHTBLUE_EX + "\nDia: ", date(), Fore.LIGHTBLUE_EX + "\nNombre: ", subject[num])
-    else:
-        print(Fore.RED + "\n❌Error al guardar los datos")
-
-def check_date():
-    init(autoreset=True)
-    if os.path.exists("person.json"):
-        if datetime.datetime.now().weekday() == 5:
-            with open("person.json", "r", encoding="utf-8") as file:
-                data = json.load(file)
-                for i in data:
-                  data[i]["faltas"] = []
-                  data[i]["dia"] = []
-                  data[i]["num_de_faltas"] = 0
-                with open("person.json", "w", encoding="utf-8") as file:
-                    json.dump(data, file, indent=4, ensure_ascii=False)
-                print(Fore.GREEN + "\n✅ Datos eliminados con éxito")
-        else:
-            return 0
-
-def loser_of_week():
-    if os.path.exists("person.json"):
-        if datetime.datetime.now().weekday() == 5:
-            with open("person.json", "r", encoding="utf-8") as file:
-                data = json.load(file)
-            for i in range(len(data)):
-                valid = False
-                if data[subject[i]]["num_de_faltas"] == 0:
-                    if i == 4 and valid == False:
-                        messagebox.showinfo("Perdedor de la semana", "No hay perdedor de la semana")
-                        print(Fore.GREEN + ("✅ No hay perdedores esta semana"))
-                    else:
-                        continue
-                else:
-                    if data[subject[i]]["num_de_faltas"] == max(data[subject[i]]["num_de_faltas"] for i in range(len(subject))):
-                        messagebox.showinfo("Perdedor de la semana",f"El perdedor de la semana es {subject[i]} con {data[subject[i]]['num_de_faltas']} falta/s")
-                        print(Fore.GREEN + "\n✅ El perdedor de la semana es ", subject[i], "con", data[subject[i]]["num_de_faltas"], "falta/s")
-                        return valid == True
-        else:
-            print(Fore.YELLOW + "\n⚠️ No es fin de semana, no se puede determinar el perdedor de la semana")
-            return
-    else:
-        messagebox.showinfo("Error" , "El Archivo json no esta creado")
-        print(Fore.RED + "❌ Archivo json no esta creado")    
-
+   
 def date():
    return datetime.datetime.now().strftime("%A %d de %B de %Y")
  
 def fault_details():
    return textbox.get("1.0", tk.END).strip()
 
-def increment_number():
-   return len(table.get_children())
-
-def ask():
-    if fault_details() == "":
-       messagebox.showerror("Error", "No se ha escrito nada.")
-       root1.destroy()
-    else:
-        answer = messagebox.askyesno("Confirmar", f"¿Quieres añadir la falta '{textbox.get('1.0', tk.END).strip()}'?")
-        if answer:
-            table.insert("", "end", values=(date(), fault_details(), increment_number() + 1)) 
-            save_json()
-            root1.destroy()
-        else:
-           messagebox.showinfo("Cancelado", "No se ha añadido la falta.")           
-           root1.destroy() 
-
-def add_falta(table):
-    global textbox, root1
-    root1 = tk.Toplevel()
-    root1.title("Añadir falta")
-    root1.geometry("700x500")
-    root1.resizable(False, False)
-
-    label = tk.Label(root1, text="Elige", font=("Arial", 16))
-    label.pack(pady=20)
-
-    frame_buttons = tk.Frame(root1)
-    frame_buttons.pack(pady=10)
-
-    imagen1 = Image.open("imagen1.jpg")
-    imagen1 = imagen1.resize((100, 100))
-    imagen1 = ImageTk.PhotoImage(imagen1)
-    happy_face = tk.Button(root1, image=imagen1)
-    happy_face.image = imagen1
-    happy_face.pack(side="top", anchor="center", padx=10)
-
-    imagen2 = Image.open("imagen2.png")
-    imagen2 = imagen2.resize((100, 100))
-    imagen2 = ImageTk.PhotoImage(imagen2)
-    sad_face = tk.Button(root1, image=imagen2)
-    sad_face.image = imagen2
-    sad_face.pack(side="top", anchor="center",padx=10)
-    frame_buttons.pack(pady=10)
-    frame_buttons.pack_forget()
-
-    textbox = tk.Text(root1, height=10, width=50)
-    textbox.pack(pady=10)
-
-    boton = tk.Button(root1, text="Añadir", command=lambda:(ask()))
-    boton.pack(pady=10)
-
-    root1.mainloop()
-
-if __name__ == "__main__":
-      add_falta()
-
-def create_window(numero):
-    global table, root, num
-    num = numero
-    root = ttk.Window()
-    root.title(subject[num])
-    root.geometry("650x450")
+def add_falta():        
+    global textbox, root, value, photo, photo2
+    root = ttk.Toplevel()
+    root.title("Añadir falta")
+    root.geometry("700x500")
     root.resizable(False, False)
 
-    label = tk.Label(root, text="Faltas de la semana", font=("Arial", 16), bg="lightblue")
+    label = ttk.Label(root, text="Elige tipo de cara:", font=("Arial", 16))
     label.pack(pady=20)
 
-    table = ttk.Treeview(root, columns=("Fecha", "Falta", "Numero de falta"), show="headings")
-    table.heading("Fecha", text="Fecha")
-    table.heading("Falta", text="Falta")
-    table.heading("Numero de falta", text="Numero de falta")
-    table.column("Fecha", width=200, anchor="center")
-    table.column("Falta", width=200, anchor="center")   
-    table.column("Numero de falta", width=200, anchor="center")
-    table.pack(pady=10)
+    value = tk.BooleanVar()
 
-    with open("person.json", "r", encoding="utf-8") as file:
-        data = json.load(file)
-    for i in range(len(data[subject[num]]["faltas"])):
-        table.insert("", "end", values=(data[subject[num]]["dia"][i], data[subject[num]]["faltas"][i], i + 1))
-    with open("person.json", "w", encoding="utf-8") as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)
-       
-    boton1 = tk.Button(root, text="Añadir falta", command=lambda:add_falta(table))
-    boton1.pack(pady=10)
+    button_frame = tk.Frame(root)
+    button_frame.pack(side="top", pady=20)
 
-    check_date()
+    image = Image.open("imagen1.jpg").resize((100, 100))
+    photo = ImageTk.PhotoImage(image)
+    image2 = Image.open("imagen2.png").resize((100, 100))
+    photo2 = ImageTk.PhotoImage(image2)
 
-    root.mainloop()
+    button1 = tk.Button(button_frame, image=photo, command=lambda:(value.set(True), print(value.get())))
+    button1.pack(side="left", padx=10)
+
+    button2 = tk.Button(button_frame, image=photo2, command=lambda:(value.set(False), print(value.get())))
+    button2.pack(side="left", padx=10)
+
+    textbox = tk.Text(root, height=10, width=50)
+    textbox.pack(pady=10)
+
+    boton = tk.Button(root, text="Añadir", command=lambda:())
+    boton.pack(pady=10)
+
+    root.wait_window(root)
--- script.py
+++ script.py
@@ -6,29 +6,32 @@ import os
 from ttkbootstrap import Style
 
 def return_main():
-    return main_window()    
+    return main_window()  
 
+def style():
+    style = Style(theme="darkly")
+    style.configure("TButton", font=("Arial", 12), padding=10)
+    style.configure("TLabel", font=("Arial", 14), padding=10)
+    style.configure("Treeview", rowheight=40)
+    return style
+    
 def main_window():
-    lib.loser_of_week()
-    lib.check_date()
+    global root
     if not os.path.exists("person.json"):
         lib.json_data() 
-    global root
-    root = ttk.Window(themename="darkly")
+    root = ttk.Window()
+    style()
     root.title("Ventana Principal")
     root.geometry("700x600")    
     root.resizable(False, False)
     label = ttk.Label(root, text="Bienvenido a faltas semanales", font=("", 20), anchor="center")
     label.pack(pady=20)
 
-    style = Style()
-    style.configure("Treeview", rowheight=40)
-
     table = ttk.Treeview(root, columns=("Nombre", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes"), show="headings", height=10)
     for col in table["columns"]:
         table.heading(col, text=col)
         table.column(col, anchor="center", width=100)
-        table.pack(pady=20)
+    table.pack(pady=20)
 
     with open("person.json", "r") as file:
         data = json.load(file)
@@ -36,11 +39,10 @@ def main_window():
         table.insert("", "end", values=data[person]["name"])
     table.pack(pady=20)
 
-    button = ttk.Button(root, text="Añadir falta", command=lambda:lib.add_falta(table))
+    button = ttk.Button(root, text="Añadir falta", command=lambda:(root.withdraw(), lib.add_falta(), root.deiconify()))
     button.pack(pady=10)
 
     root.mainloop()
-
 
 if __name__ == "__main__":
     main_window()
